b6c6de8d4caf30039d428eaf4852353557e6652a
Grronn <84620898+Grronn@users.noreply.github.com>
2023-11-24 23:41:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import re
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoProcessor, AutoModel
 import streamlit as st
+
 WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 st.title("Сокращение текста и прослушивание результата")
-article_text=st.text_input('Введите текст и нажмите Enter')
+article_text=st.text_area('Введите текст и нажмите Enter', height=400)
 #article_text = """Videos that say approved vaccines are dangerous and cause autism, cancer or infertility are among those that will be taken down, the company said.  The policy includes the termination of accounts of anti-vaccine influencers.  Tech giants have been criticised for not doing more to counter false health information on their sites.  In July, US President Joe Biden said social media platforms were largely responsible for people's scepticism in getting vaccinated by spreading misinformation, and appealed for them to address the issue.  YouTube, which is owned by Google, said 130,000 videos were removed from its platform since last year, when it implemented a ban on content spreading misinformation about Covid vaccines.  In a blog post, the company said it had seen false claims about Covid jabs "spill over into misinformation about vaccines in general". The new policy covers long-approved vaccines, such as those against measles or hepatitis B.  "We're expanding our medical misinformation policies on YouTube with new guidelines on currently administered vaccines that are approved and confirmed to be safe and effective by local health authorities and the WHO," the post said, referring to the World Health Organization."""
 
 model_name = "csebuetnlp/mT5_multilingual_XLSum"
